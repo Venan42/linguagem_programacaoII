0efc1d2c9a9f3ab5399af8ae31159265c43e91c4
Vinícius <viniciusandradebc@gmail.com>
2025-06-02 13:43:41 -0300

    Adicionando responsta da segunda questão

--- lista03/questao02/Util.java
+++ lista03/questao02/Util.java
@@ -4,23 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Util {
-	public static int getNumeroReverso(int n) {
-		int ultimoDigito;
-		int nReverso = 0;
-		
-		while (n != 0) {    
-			ultimoDigito = n % 10;
-	    	nReverso = nReverso * 10 + ultimoDigito;
-		    n = n / 10; 
-		}
-		
-		return nReverso;
-	}
-
 	public static void main(String[] args) {
 		List<Integer> numeros1 = new ArrayList<Integer>();
 		List<Integer> numeros2 = new ArrayList<Integer>();
-		int numeroReverso;
+		int ultimoDitigo, primeiroDigito;
 
 		numeros1.add(1234);
 		numeros1.add(870);
@@ -28,22 +15,25 @@ public class Util {
 		numeros1.add(349893);
 		numeros1.add(2343);
 		numeros1.add(5555555);
+		numeros1.add(9090);
 		numeros1.add(909);
-		numeros1.add(9865);
 		numeros1.add(234);
 		numeros1.add(11);
 
-		for(int numero : numeros1){
-			numeroReverso = getNumeroReverso(numero);
-			while(numero < 10){
-				numero /= 10;
+		for(Integer numero : numeros1){
+			ultimoDitigo = numero % 10;
+			primeiroDigito = numero;
+			while(primeiroDigito > 10){
+				primeiroDigito /= 10;
 			}
-			while(numeroReverso < 10){
+			if(ultimoDitigo == primeiroDigito)
+				numeros2.add(numero);
+		}
+		for (Integer numero : numeros2){
+			numeros1.remove(numero);
+		}
 
-			}
-		}
-		for (String animal5oumaisletras : numeros2){
-			numeros1.remove(animal5oumaisletras);
-		}
+		System.out.println("Numeros que não tem o primeiro e ultimo dígitos iguais: " + numeros1);
+		System.out.println("Numeros que tem o primeiro e utlimo dígitos iguais: " + numeros2);
 	}
 }
